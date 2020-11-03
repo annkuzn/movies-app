eaacd722a4c3073012cd72fbcfd72b7dfc41780a
Anna Kuzn <annbuho@gmail.com>
2020-11-03 12:39:41 +0300

    feat: creating a component DataMovies

---
+++ src/services/dataMovies.js
@@ -0,0 +1,23 @@
+
+
+export default class DataMovies {
+
+    apiBase = 'https://api.themoviedb.org/3/';
+
+    apiKey = '88a91ac04dba011fd615caf2589ff8fb';
+
+    async getResource(url, query, page) {
+       
+        const res = await fetch(`${this.apiBase}${url}?api_key=${this.apiKey}&query=${query}&page=${page}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
+        }
+
+        return res.json();
+    }
+    
+    getMovies(query, page) {
+        return this.getResource('search/movie', query, page).then(res => res.results)
+    }
+}
