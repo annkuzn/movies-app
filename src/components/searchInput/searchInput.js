38efe174ee5ad4e52c783e753a1d2fe16a333a84
Anna Kuzn <annbuho@gmail.com>
2020-12-02 15:13:00 +0300

    refactor: moved render condition into App

--- src/components/searchInput/searchInput.js
+++ src/components/searchInput/searchInput.js
@@ -4,28 +4,26 @@ import { Input } from 'antd';
 import debounce from 'lodash.debounce';
 
 
-const SearchInput = ({ updateRequest, tab }) => {
+const SearchInput = ({ updateRequest }) => {
 
     const updateRequestDebounce = debounce(updateRequest, 700);
 
-    return tab === 1 ? (
+    return (
         <Input 
             className="search-input" 
             placeholder='Type to search...' 
             onInput={updateRequestDebounce} 
             autoFocus
         />
-    ) : null;
+    )
 };
 
 export default SearchInput;
 
 SearchInput.defaultProps = {
-    tab: 1,
     updateRequest: (() => {})
 };
 
 SearchInput.propTypes = {
-    tab: PropTypes.number,
     updateRequest: PropTypes.func
 };
